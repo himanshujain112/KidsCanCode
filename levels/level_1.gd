18a1375dc583dd026bf6f517a9575720032d9e19
Himanshu Jain <Himanshujain82669@gmail.com>
2025-02-17 17:19:20 +0530

    commits

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -1,17 +1,13 @@
 extends Node
 
-@onready var player = $player
+@onready var player = $AnimationPlayer
 @onready var command_panel = $CommandPanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	command_panel.hidden = true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	command_panel.visible = false
 
 
 func _on_execute_button_pressed() -> void:
-	#
-	pass
+	player.play("cmdPanelslidein")
+	await player.animation_finished
